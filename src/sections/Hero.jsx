cccbda1d8d3c999ced8ffa53808fc2b49ce435c6
innocent <workinhim@gmail.com>
2026-02-19 17:33:00 +0100

    working on profile photo, navbar scroll transition, Technologies i work with, down bancing arrow

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,23 @@
 import { Button } from "@/components/Button";
-import { ArrowRight, Download, Github, Linkedin, Twitter } from "lucide-react";
+import {
+  ArrowRight,
+  Download,
+  ChevronDown,
+  Github,
+  Linkedin,
+  Twitter,
+} from "lucide-react";
 import { AnimatedBorderButton } from "@/components/AnimatedBorderButton";
+
+const skills = [
+  "JavaScript",
+  "Tailwind CSS",
+  "React",
+  "Next.js",
+  "TypeScript",
+  "Git",
+  "GitHub",
+];
 
 export const Hero = () => {
   return (
@@ -96,21 +113,61 @@ export const Hero = () => {
             {/* Profile image */}
             <div className="relative max-w-xs mx-auto">
               <div
-                className="absolute inset-0 rounded-3xl bg-gradient-to-br from-primary/30
+                className="absolute inset-0 runded-3xl bg-gradient-to-br from-primary/30
                via-transparent to-primary/10 blur-2xl animate-pulse"
               />
               <div className="relative glass rounded-3xl p-2 glow-border">
                 <img
-                  src="/profile01.png"
+                  src="/profile-02.png"
                   alt="Izuchukwu Innocent"
-                  className="w-full aspect-[4/6] object-cover rounded-3xl shadow-xl shadow-primary"
+                  className="w-full aspect-[3/5] object-cover rounded-3xl shadow-xl shadow-primary"
                 />
                 {/* Floating Badge */}
+                <div className="absolute -bottom-4 -right-4 glass rounded-xl px-4 py-3 animate-float">
+                  <div className="flex items-center gap-3">
+                    <div className="w-3 h-3 bg-red-100 rounded-full animate-pulse" />
+                    <span className="text-md font-medium">
+                      Available for work
+                    </span>
+                  </div>
+                </div>
                 {/* State Badge */}
+                <div className="absolute -top-4 -left-4 glass rounded-xl px-4 py-3 animate-float animation-delay-500">
+                  <div className="text-2xl font-bold text-primary">2+</div>
+                  <div className="text-xs text-gray-300">Year Exp.</div>
+                </div>
               </div>
             </div>
           </div>
-          <div></div>
+        </div>
+        {/* skills Section */}
+        <div className="mt-20 animate-fade-in animation-delay-600">
+          <p className="text-sm text-muted-foreground mb-6 text-center">
+            Technologies I work with
+          </p>
+          <div className="relative overflow-hidden">
+            <div className="flex animate-marquee">
+              {[...skills, ...skills].map((skill, idx) => (
+                <div key={idx} className="flex-shrink-0 px-8 py-4">
+                  <span className="text-xl font-semibold text-muted-foreground hover:text-amber-50 transition-colors">
+                    {skill}
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div
+          className="absolute bottom-8 left-1/2 -translate-x-1/2
+        animate-fade-in animation-delay-800"
+        >
+          <a
+            href="#about"
+            className="flex flex-col items-center gap-2 text-muted-foreground hover:text-primary"
+          >
+            <span className="text-xs uppercase tracking-wider">Scroll</span>
+            <ChevronDown className="w-6 h-6 animate-bounce" />
+          </a>
         </div>
       </div>
     </section>
