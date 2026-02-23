4fa9a926bf47363076125063e8b39ead19d346cf
innocent <workinhim@gmail.com>
2026-02-23 17:49:52 +0100

    feat(contact): implement responsive contact section with form validation and user feedback states

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,3 +1,129 @@
+import { Mail, MapPin, Phone, Send } from "lucide-react";
+import { Button } from "@/components/Button";
+import { useState } from "react";
+
+const contactInfo = [
+  {
+    icon: Mail,
+    label: "Email",
+    Value: "workinhim@gmail.com",
+    href: "mailto:workinhim@gmail.com",
+  },
+  {
+    icon: Phone,
+    label: "Phone",
+    Value: "+234 (803) 088-5144",
+    href: "tel:+2348030885144",
+  },
+  {
+    icon: MapPin,
+    label: "Location",
+    Value: "Nigeria, Anambra",
+    href: "#",
+  },
+];
+
 export const Contact = () => {
-  return <section></section>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+  return (
+    <section id="contact" className="py-32 relative overflow-hidden">
+      {/* Bg glows */}
+      <div className="absolute top-1/4 right-0 w-96 h-96 bg-primary/5 rounded-full blur-3xl" />
+      <div className="absolute bottom-1/4 left-0 w-64 h-64 bg-highlight/5 rounded-full blur-3xl" />
+      <div className="cointainer mx-auto px-6 relative z-10">
+        {/* Section Header */}
+        <div className="text-center mx-auto max-w-3xl mb-16">
+          <span className="text-secondary-foreground text-sm font-medium tracking-wider uppercase animate-face-in">
+            Get in Touch
+          </span>
+          <h2 className="text-4xl md:text-5xl font-bold mt-4 mb-6 animate-fade-in animation-delay-100 text-secondary-foreground">
+            Let's build
+            <span className="font-serif italic font-normal text-white">
+              {" "}
+              something great
+            </span>
+          </h2>
+          <p className="text-muted-foreground animate-fade-in animation-delay-200">
+            Have a project in mind? I'd love to hear about it. Send me a message
+            and let's discuss how we can work together.
+          </p>
+        </div>
+
+        <div className="grid lg:grid-cols-2 gap-12 max-w-5xl mx-auto">
+          <div className="glass p-8 rounded-3xl bg-foreground/5 border border-primary/30 animate-delay-300">
+            <form className="space-y-6" onSubmit={handleSubmit} >
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-mediummb-2"
+                />
+                Name
+                <input
+                  id="name"
+                  type="text"
+                  required
+                  placeholder="Your name..."
+                  value={formData.name}
+                  onChange={(e) =>
+                    setFormData({ ...formData, name: e.target.value })
+                  }
+                  className="w-full px-4 py-3 bg-surface rounded-xl border border-border focus:border-primary focus:ring-1 focus:ring-primary outline-none transition-all"
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="email"
+                  typeof="email"
+                  className="block text-sm font-mediummb-2"
+                />
+                Email
+                <input
+                  required
+                  placeholder="Your@email.com"
+                  className="w-full px-4 py-3 bg-surface rounded-xl border border-border focus:border-primary focus:ring-1 focus:ring-primary outline-none transition-all"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-mediummb-2"
+                >
+                  Message
+                </label>
+                <textarea
+                  rows={5}
+                  required
+                  placeholder="Your message..."
+                  className="w-full px-4 py-3 bg-surface rounded-xl border border-border focus:border-primary focus:ring-1 focus:ring-primary outline-none transition-all resize-none"
+                  value={formData.message}
+                  onChange={(e) =>
+                    setFormData({ ...formData, message: e.target.value })
+                  }
+                />
+              </div>
+
+              <Button className="w-full" type="submit" size="lg">
+                Send Message
+                <Send />
+              </Button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
